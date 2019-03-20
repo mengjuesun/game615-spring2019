fa13b4c47cc6599e3cfddbaadc93ecb81163f037
mengjuesun <ms6729a@student.american.edu>
2019-03-20 00:11:07 -0400

    midterm

---
+++ exercises/midterm/Assets/Gameplay/Script/GameStart.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GameStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject btnObj = GameObject.Find("Button");
+        Button btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            this.GoNextScene(btnObj);
+
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoNextScene(GameObject NScene)
+    {
+        Application.LoadLevel("MainScene");
+    }
+}
+
---
+++ exercises/midterm/Assets/Gameplay/Script/PickUps.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PickUps : MonoBehaviour
+{
+
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 5)
+        {
+            winText.text = "You Are The Greatest Wizard!";
+        }
+    }
+}
